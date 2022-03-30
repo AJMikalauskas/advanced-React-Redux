a585173a61e1335623da18f48357d3bd5a46f91a
AJMikCode <ajmikalauskas@hotmail.com>
2022-03-29 21:32:22 -0500

    created Action Creator Thunk which was really just  a special exported function from a slice but after the whole createSlice  in cartItemSliceLogic.js 2. This function returned a function which replaced what was previously in App.js of the fetch call and notifications and moved it all into the function from the cartItemSliceLogic.js 3. This function is dispatched in App.js with a param of the cartItemSliceLogic which contains the cart Items array, overall quantity, and overall cost.

--- src/App.js
+++ src/App.js
@@ -6,11 +6,13 @@ import { Fragment, useEffect, useState } from 'react';
 import ShoppingCartModal from './components/UI/Modal';
 import Notification from './components/UI/Notification';
 import { showingCartModalActions } from './store/modal';
+import {sendCartData } from "./cartItemSliceLogic";
 
 // This is so the request isn't sent on the start of the page, not what I would expect 
 let isInitial = true;
 
 function App() {
+  const dispatch = useDispatch();
   const testRedux = useSelector(cartModalTest => cartModalTest.cartModal.showingCart);
   const cartAndProductsComponents = <Fragment>
     <Cart />
@@ -25,59 +27,73 @@ function App() {
   // useState for notifications, attempt on my own
   // const [isLoadingReq, setisLoadingReq] = useState(false);
   // const [ifError, setIfError] = useState();
-  const dispatch = useDispatch();
+  //const dispatch = useDispatch();
   const notification = useSelector(state => state.cartModal.notification);
   useEffect(() => {
+    // Call sendCartData function from cartItemSliceLogic and use useSelecotr function to get and send up with the cart data
+      // The cartData contains the cartItems array, overallCost and overall quantity
+
+      if(isInitial) 
+      {
+        // set to false so http calls are only made on adding of items just not on start of page
+        isInitial = false;
+        return;
+      }
+      dispatch(sendCartData(cartStateTracker));
+
     // This if statement didn't work because if the cart item has o amount it will be removed, which means the PUT req will never get called
       // and considering, even if it was called, it would still result in the same error of having totalCost and totalQuantity as 0
         // when we want overall deletion from the whole DB if 0 exist -> internet solution is action creator thunks
     // if(cartStateTracker.cartItems.length >= 0) {
 
-    const sendCartData = async() => {
+  //   const sendCartData = async() => {
 
-      // Show original starting loading notification to alert user that the request is being made
-        // Could handle notifcations state and JSX conditionals in the return below to show notifcations or do his way
-          // useState for start loading notification
-    // setisLoadingReq(true);
-    dispatch(showingCartModalActions.showNotification({ status: "Pending...",title: "Request Is Loading", message: "Your Request is being processed!"}));
-    //console.log(notification.status);
-    const response = await fetch("https://advanced-redux-a16d1-default-rtdb.firebaseio.com/cartItems.json",
-    {
-      method: 'PUT',
-      headers: {'Content-Type': 'application/json'},
-      body: JSON.stringify(cartStateTracker)
-    });
+  //     // Show original starting loading notification to alert user that the request is being made
+  //       // Could handle notifcations state and JSX conditionals in the return below to show notifcations or do his way
+  //         // useState for start loading notification
+  //   // setisLoadingReq(true);
+    
+  //   //This is an auto created action creator thunk by using dispatch and calling a reducer action through it
+  //     // But we can create our own so this isn't the end reuslt of lecture #260
+  //   dispatch(showingCartModalActions.showNotification({ status: "Pending...",title: "Request Is Loading", message: "Your Request is being processed!"}));
+  //   //console.log(notification.status);
+  //   const response = await fetch("https://advanced-redux-a16d1-default-rtdb.firebaseio.com/cartItems.json ",
+  //   {
+  //     method: 'PUT',
+  //     headers: {'Content-Type': 'application/json'},
+  //     body: JSON.stringify(cartStateTracker)
+  //   });
 
-    if(!response.ok)
-    {
-      // Put error notificatio in here if fail occurs
-      //setIfError(true);
+  //   if(!response.ok)
+  //   {
+  //     // Put error notificatio in here if fail occurs
+  //     //setIfError(true);
 
-      // Don't dispatch error here because other errors can occur in the fetch request, so this may not be as useful as if you .catch()
-        // which will catch any errors from the whole async function.
-      //dispatch(showingCartModalActions.showNotification({status: 'error',title: "Request Failed!", message: "Your Request failed to Load due to an error!"}));
-      throw new Error('Sending Cart Data Failed!');
-    }
-    // if success display notifcation of success
-    //const respData = await response.json();
-    dispatch(showingCartModalActions.showNotification({status: 'success',title: "Request Succeeded!", message: "Your Request Succeeded in Loading!"}));
-    // setisLoadingReq(false);
-    // setIfError(false);
-  }
-  // .catch() is able to catch an error in the overall function, ratehr than only catching an error after the fetch call,
-    // it's better to do so via the .catch() method 
-      // Stop the data or function from being called and sending request if isInitial is tru, outside component so value doesn't rerender on component on rerender
-    if(isInitial) 
-    {
-      // set to false so http calls are only made on adding of items just not on start of page
-      isInitial = false;
-      return;
-    }
+  //     // Don't dispatch error here because other errors can occur in the fetch request, so this may not be as useful as if you .catch()
+  //       // which will catch any errors from the whole async function.
+  //     //dispatch(showingCartModalActions.showNotification({status: 'error',title: "Request Failed!", message: "Your Request failed to Load due to an error!"}));
+  //     throw new Error('Sending Cart Data Failed!');
+  //   }
+  //   // if success display notifcation of success
+  //   //const respData = await response.json();
+  //   dispatch(showingCartModalActions.showNotification({status: 'success',title: "Request Succeeded!", message: "Your Request Succeeded in Loading!"}));
+  //   // setisLoadingReq(false);
+  //   // setIfError(false);
+  // }
+  // // .catch() is able to catch an error in the overall function, ratehr than only catching an error after the fetch call,
+  //   // it's better to do so via the .catch() method 
+  //     // Stop the data or function from being called and sending request if isInitial is tru, outside component so value doesn't rerender on component on rerender
+    // if(isInitial) 
+    // {
+    //   // set to false so http calls are only made on adding of items just not on start of page
+    //   isInitial = false;
+    //   return;
+    // }
       
-  sendCartData().catch((error) => {
-    dispatch(showingCartModalActions.showNotification({status: 'error',title: "Request Failed", message: error.message}));
-  }
-  )
+  // sendCartData().catch((error) => {
+  //   dispatch(showingCartModalActions.showNotification({status: 'error',title: "Request Failed", message: error.message}));
+  // }
+  // )
   // }
     //cartStateTracker
   }, [cartStateTracker, dispatch]);
--- src/cartItemSliceLogic.js
+++ src/cartItemSliceLogic.js
@@ -1,72 +1,147 @@
+import { useSelector } from "react-redux";
 import { createSlice } from "@reduxjs/toolkit";
+import { showingCartModalActions } from "./store/modal";
 
 const cartItemSlice = createSlice({
-    name: 'cartItemsLogic',
-    initialState: {
-        cartItems: [],
-        totalOverallAmount: 0,
-        totalCostAmt: 0
+  name: "cartItemsLogic",
+  initialState: {
+    cartItems: [],
+    totalOverallAmount: 0,
+    totalCostAmt: 0,
+  },
+  reducers: {
+    addItem(state, action) {
+      // This is very different from context already since you can push it on without changes or errors being caused
+      // This looks very different from the reducer functions because the ... operator is used more and is used to store
+      // and then adding an item, you can't and shouldn't change the states at all and if adding you use .concat() and not .push()
+
+      // Redux handles it so .push() and the ... operator don't have to be used (what I would consider incessantly).
+      // No manipulation of old state due to redux toolkit
+
+      // newItem would store specific properties, send up as object with price, name, and more
+      const newItem = action.payload;
+      console.log(newItem);
+      // adding check for an existingItem with the same id
+      const existingItem = state.cartItems.find(
+        (item) => item.itemId === newItem.itemId
+      );
+      //console.log(existingItem);
+      if (!existingItem) {
+        // Include the specific properties of the newItem Object -> not just .push(newItem),
+        //properties include name, id, price, quantity/amount and overall price of item(s)
+        state.cartItems.push({
+          itemName: newItem.itemName,
+          itemId: newItem.itemId,
+          price: newItem.price,
+          amount: 1,
+          totalPrice: newItem.price,
+        });
+        state.totalOverallAmount += 1;
+        state.totalCostAmt += newItem.price;
+      } else {
+        // Once again you don't have to store the old data in a new variable and update something beyond the state
+        // This can be kind of confusing considering the fact the food oredering app was 2 sections ago and not adjusting state is still resonating
+        // While to adjust state with redux is perfectly ok.
+        existingItem.amount += 1;
+        existingItem.totalPrice += newItem.price;
+        state.totalOverallAmount += 1;
+        state.totalCostAmt += newItem.price;
+      }
+      //const lastSnapshotOfCart = state.cartItems;
     },
-    reducers: {
-        addItem(state, action) {
-            // This is very different from context already since you can push it on without changes or errors being caused 
-                // This looks very different from the reducer functions because the ... operator is used more and is used to store 
-                    // and then adding an item, you can't and shouldn't change the states at all and if adding you use .concat() and not .push()
+    removeItem(state, action) {
+      // The action here is most likely an id, but is only really useful if multiple items exist overall
+      // Option to remove and then run if the amount is 0, than to remove item completely by slice?
+      const removingItemId = action.payload;
+      const removeExistingItem = state.cartItems.find(
+        (item) => item.itemId === removingItemId.itemId
+      );
+      //Item Specific Amount and Price Removals
+      removeExistingItem.amount -= 1;
+      removeExistingItem.totalPrice -= removeExistingItem.price;
 
-                    // Redux handles it so .push() and the ... operator don't have to be used (what I would consider incessantly).
-                        // No manipulation of old state due to redux toolkit
+      //Overall/Total Amount and Price Removals
+      state.totalOverallAmount -= 1;
+      state.totalCostAmt -= removeExistingItem.price;
+      //const newCartItems = cartItems.filter((cartItem) => cartItem.amount )
 
-            // newItem would store specific properties, send up as object with price, name, and more
-            const newItem = action.payload;
-            console.log(newItem);
-            // adding check for an existingItem with the same id
-            const existingItem = state.cartItems.find(item => item.itemId === newItem.itemId);
-            //console.log(existingItem);
-            if(!existingItem) {
-                // Include the specific properties of the newItem Object -> not just .push(newItem), 
-                    //properties include name, id, price, quantity/amount and overall price of item(s)
-                state.cartItems.push({itemName: newItem.itemName,itemId: newItem.itemId, price: newItem.price, amount: 1, totalPrice: newItem.price});
-                state.totalOverallAmount += 1;
-                state.totalCostAmt += newItem.price;
-            }
-            else
-            {
-                // Once again you don't have to store the old data in a new variable and update something beyond the state
-                    // This can be kind of confusing considering the fact the food oredering app was 2 sections ago and not adjusting state is still resonating
-                        // While to adjust state with redux is perfectly ok.
-                existingItem.amount += 1;
-                existingItem.totalPrice += newItem.price;
-                state.totalOverallAmount += 1;
-                state.totalCostAmt += newItem.price;
-            }
-            //const lastSnapshotOfCart = state.cartItems;
-        },
-        removeItem(state,action) {
-            // The action here is most likely an id, but is only really useful if multiple items exist overall
-                // Option to remove and then run if the amount is 0, than to remove item completely by slice?
-            const removingItemId = action.payload;
-            const removeExistingItem = state.cartItems.find(item => item.itemId === removingItemId.itemId);
-            //Item Specific Amount and Price Removals
-            removeExistingItem.amount -= 1;
-            removeExistingItem.totalPrice -= removeExistingItem.price;
-
-            //Overall/Total Amount and Price Removals
-            state.totalOverallAmount -= 1;
-            state.totalCostAmt -= removeExistingItem.price;
-            //const newCartItems = cartItems.filter((cartItem) => cartItem.amount )
-
-            // This is probably the simplest way to write this, only runs through if statement if the amount is 0 rather than filtering for no reason
-            if(removeExistingItem.amount === 0)
-            {
-                // This filtered solution is probably simpler and quicker than to run an (for and if loop) to find the id and then splice the element from the array
-                const newCartItems = state.cartItems.filter((cartItem) => cartItem.amount > 0);
-                // This statement below is only possible through redux
-                state.cartItems = newCartItems;
-            } 
-        }
-    }
+      // This is probably the simplest way to write this, only runs through if statement if the amount is 0 rather than filtering for no reason
+      if (removeExistingItem.amount === 0) {
+        // This filtered solution is probably simpler and quicker than to run an (for and if loop) to find the id and then splice the element from the array
+        const newCartItems = state.cartItems.filter(
+          (cartItem) => cartItem.amount > 0
+        );
+        // This statement below is only possible through redux
+        state.cartItems = newCartItems;
+      }
+    },
+  },
 });
 
+// This is to create an action creator thunk, moved this function name from App.js to here -> redux auto makes these by reducer functions above such as
+// removeItem() and addItem()
+// could reutnr object like one's above but you can also return a function from a function with dispatch param
+export const sendCartData = (cartStateTracker) => {
+    //const cartStateTracker = useSelector(state => state.cartItemLogic);
+  //return{ type: '', payload:}
+  // can return as async anonymous function or cretae new async function in the return function and put async there  
+    // I ended up making this async due to the try catch of the sendRequest nested function
+        // Within the try catch, the try will call the function overall and if it succeeds, call dispatch to succesful notification
+            // if catch is called, send error notification
+
+    // instantly once this function is called, returns a function which shows the notification of a loading State notification
+        // Send request via async/await fetch call. it also runs a try catch call after the overall sendRequest function is called
+            // if try is succesful, sends success notification else fail exists and error notification shows
+  return async(dispatch) => {
+    dispatch(
+      showingCartModalActions.showNotification({
+        status: "Pending...",
+        title: "Request Is Loading",
+        message: "Your Request is being processed!",
+      })
+    );
+
+    // Need async function this is just the 2nd option as mentioned above
+    const sendRequest = async() => {
+        //const cartStateTracker = useSelector(state => state.cartItemLogic);
+    const response = await fetch("https://advanced-redux-a16d1-default-rtdb.firebaseio.com/cartItems.json ",
+    {
+      method: 'PUT',
+      headers: {'Content-Type': 'application/json'},
+      // This is just a minor useSelector(), stores the cartItemLogic which contains the overallquantity, overallCost, and cartItems Array
+        // sisnce it has a react hook it can only be contained within a react component or customm hook, how to use here?
+            // we will pass these cartStateTracker via the sendCartData function, call sendCartData with correct logic of the cart from this
+                // can't use useSelector() in this slice.
+      body: JSON.stringify(cartStateTracker)
+    });
+
+    if(!response.ok)
+    {
+      // Put error notificatio in here if fail occurs
+      //setIfError(true);
+
+      // Don't dispatch error here because other errors can occur in the fetch request, so this may not be as useful as if you .catch()
+        // which will catch any errors from the whole async function.
+      //dispatch(showingCartModalActions.showNotification({status: 'error',title: "Request Failed!", message: "Your Request failed to Load due to an error!"}));
+      throw new Error('Sending Cart Data Failed!');
+    }
+};
+
+try {
+    // Can only be possible of await if the return function is async so this can be await.
+        // if succeed and continues request after function call, send success notification
+            // elese if catch send fail notification
+    await sendRequest();
+
+    dispatch(showingCartModalActions.showNotification({status: 'success',title: "Request Succeeded!", message: "Your Request Succeeded in Loading!"}));
+}
+catch(error) {
+    dispatch(showingCartModalActions.showNotification({status: 'error',title: "Request Failed", message: error.message}));
+}
+    //console.log(notification.status);
+    //dispatch();
+  };
+};
 
 export const cartItemSliceActions = cartItemSlice.actions;
 export default cartItemSlice;
